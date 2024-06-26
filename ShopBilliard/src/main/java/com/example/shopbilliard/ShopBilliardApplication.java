3450663f0e630ab584e6468ddcafda58b85d0fd4
manhhhhhhh <125013750+manhhhhhhh@users.noreply.github.com>
2024-06-26 16:19:54 +0700

    Add files via upload

---
+++ ShopBilliard/src/main/java/com/example/shopbilliard/ShopBilliardApplication.java
@@ -0,0 +1,13 @@
+package com.example.shopbilliard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopBilliardApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShopBilliardApplication.class, args);
+    }
+
+}
---
+++ ShopBilliard/src/test/java/com/example/shopbilliard/ShopBilliardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.shopbilliard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopBilliardApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
